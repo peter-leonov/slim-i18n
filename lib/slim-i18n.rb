0f080dbfb1ec287b3c6d1fb03f713e893eb256e0
Peter Leonov <gojpeg@gmail.com>
2014-04-07 01:34:23 +0400

    the hook

---
+++ lib/slim-i18n.rb
@@ -0,0 +1,12 @@
+class SlimI18n < Slim::Filter
+  def on_slim_interpolate str
+    # $1: leave \\ as is for Slim's interpolation
+    # $2: treat \$ as $
+    # $3: $a.b.c form
+    # $4: ${…} form
+    stuffed = str.gsub(/(\\\\)|\\(\$)|\$([\.\w]+)|\${([^}]+)}/){ $1 || $2 || "\#{t('#{$3||$4}')}" }
+    [:slim, :interpolate, stuffed]
+  end
+end
+
+Slim::Engine.before(Slim::Interpolation, self)
